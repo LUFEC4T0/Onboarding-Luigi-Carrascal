e0d44f3b688cfac113b0b66e669caf1f59b4855b
LUFEC4T0 <luigi02190622@gmail.com>
2023-12-22 09:00:08 -0500

    Se creo repositorio y empezo a realizar los ejercicios

---
+++ Intro JS/main.js
@@ -0,0 +1,20 @@
+let myName = "Luigi"
+console.log (myName)
+
+let myLastName = "Carrascal"
+console.log (myLastName)
+
+let myAge = 18
+console.log (myAge)
+
+let myPet = "Odin"
+console.log (myPet)
+
+let myPetAge = 2
+console.log (myPetAge)
+
+let fullName = `${myName} ${myLastName}`
+console.log (fullName)
+
+let presentationText = `${myName} ${myLastName} ${myAge} ${myPet} ${myPetAge} ${fullName}`
+console.log (presentationText)
